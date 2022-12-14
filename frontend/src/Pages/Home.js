8f0e9a30a571b04980546d04559f91b876c5bdc8
Exabiier <exabiier.bars@gmail.com>
2022-12-13 23:35:39 -0600

    Finished Home, Login, and Register Pages.

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {FaQuestionCircle, FaTicketAlt} from 'react-icons/fa'
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <section className="heading">
+        <h1>What do you need help with?</h1>
+        <p>Please choose from an option below</p>
+      </section>
+
+      <Link to="/new-ticket" className="btn btn-revers btn-block"><FaQuestionCircle/> Create New Ticket</Link>
+      <Link to="/tickets" className="btn btn-revers btn-block"><FaTicketAlt/> View My tickets</Link>
+    </>
   )
 }
 

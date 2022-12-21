ce3522de90385249eafe82391f189c89a77c2335
Exabiier <exabiier.bars@gmail.com>
2022-12-21 16:59:17 -0600

    Made New-ticket form and made basic layout for tickets in Redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import{BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+
+///////////// Pages ////////////
 import Home from './Pages/Home';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
 import Header from './Layout/Header';
+import NewTicket from './Pages/New-Ticket/NewTicket';
+import PrivateRoutes from './Pages/New-Ticket/PrivateRoutes';
 
 
 function App() {
@@ -17,7 +21,11 @@ function App() {
         <Route path="/" element={<Home />}/>
         <Route path="/login" element={<Login />}/>
         <Route path="/register" element={<Register />}/>
-      
+
+        <Route path="/new-ticket" element={<PrivateRoutes />}>
+        <Route path="/new-ticket" element={<NewTicket />} />
+        </Route>
+
       </Routes>
     </div>
   </Router>
